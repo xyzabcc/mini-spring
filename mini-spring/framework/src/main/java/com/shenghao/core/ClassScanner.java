2191ba392a8bc55ca79edc37e4a63f5a801bd47f
jiaqi <jiaqi@mogujie.com>
2019-09-03 18:27:31 +0800

    code pass

--- mini-spring/framework/src/main/java/com/shenghao/core/ClassScanner.java
+++ mini-spring/framework/src/main/java/com/shenghao/core/ClassScanner.java
@@ -10,6 +10,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class ClassScanner {
+
     public static List<Class<?>> scanClass(String packageName) throws IOException, ClassNotFoundException {
         //用于保存结果的容器
         List<Class<?>> classList = new ArrayList<>();
@@ -22,24 +23,22 @@ public class ClassScanner {
 
         int index = 0;//测试
 
-        while(resources.hasMoreElements()){
+        while (resources.hasMoreElements()) {
             System.out.println(++index);//测试
 
             //拿到下一个资源
             URL resource = resources.nextElement();
             //先判断是否是jar包，因为默认.class文件会被打包为jar包
-            if(resource.getProtocol().contains("jar")){
+            if (resource.getProtocol().contains("jar")) {
 
                 System.out.println("拿到一个jar包");//测试
 
                 //把URL强转为jar包链接
-                JarURLConnection jarURLConnection = (JarURLConnection)resource.openConnection();
+                JarURLConnection jarURLConnection = (JarURLConnection) resource.openConnection();
                 //根据jar包获取jar包的路径名
                 String jarFilePath = jarURLConnection.getJarFile().getName();
                 //把jar包下所有的类添加的保存结果的容器中
                 classList.addAll(getClassFromJar(jarFilePath, path));
-            }else{//也有可能不是jar文件，先放下
-                //todo
             }
         }
         return classList;
@@ -47,18 +46,17 @@ public class ClassScanner {
 
     /**
      * 获取jar包中所有路径符合的类文件
-     * @param jarFilePath
-     * @param path
-     * @return
+     *
+     * @return list
      */
     private static List<Class<?>> getClassFromJar(String jarFilePath, String path) throws IOException, ClassNotFoundException {
         List<Class<?>> classes = new ArrayList<>();//保存结果的集合
         JarFile jarFile = new JarFile(jarFilePath);//创建对应jar包的句柄
         Enumeration<JarEntry> jarEntries = jarFile.entries();//拿到jar包中所有的文件
-        while(jarEntries.hasMoreElements()){
+        while (jarEntries.hasMoreElements()) {
             JarEntry jarEntry = jarEntries.nextElement();//拿到一个文件
             String entryName = jarEntry.getName();//拿到文件名，大概是这样：com/shenghao/test/Test.class
-            if (entryName.startsWith(path) && entryName.endsWith(".class")){//判断是否是类文件
+            if (entryName.startsWith(path) && entryName.endsWith(".class")) {//判断是否是类文件
                 String classFullName = entryName.replace("/", ".")
                         .substring(0, entryName.length() - 6);
                 classes.add(Class.forName(classFullName));
